8ac5d134da44f384ef4ba0ba8110b7a9b1beb5a0
Stefan Schuddinck <stefanschuddinck@hotmail.com>
2018-04-19 11:25:37 +0200

    a tout le monde

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("bonjour");
+System.out.println("Bonjour a tout le monde a tous mes amis");
 	}
 
 }
