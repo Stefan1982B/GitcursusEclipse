4bbe518d1d7610866580658fcaf600650b391424
Stefan1982B <36157965+Stefan1982B@users.noreply.github.com>
2018-04-19 11:35:32 +0200

    Update Main.java

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Hallo Iedereen en aan al mijn vrienden");
+System.out.println("Hallo Allemaal en aan al mijn vrienden");
 
 
 	}
